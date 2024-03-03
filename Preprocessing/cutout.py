3a53b47b96da3b3d1efe496d337bd065edfc67bb
Jason Walter <jaason@protonmail.ch>
2024-03-03 11:14:52 -0500

    Add analog column renaming script; update path parsing for cutout

--- Preprocessing/cutout.py
+++ Preprocessing/cutout.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description="Carve up RaceCapture CSV files")
 parser.add_argument('filename')
@@ -11,6 +12,5 @@ df = pd.read_csv(args.filename)
 df = df[df["Interval|\"ms\"|0|0|1"] >= args.start_interval]
 df = df[df["Interval|\"ms\"|0|0|1"] <= args.end_interval]
 
-write_name = str(args.start_interval) + "_" + str(args.end_interval) + "_" + args.filename
-
+write_name = str(args.start_interval) + "_" + str(args.end_interval) + "_" + os.path.basename(args.filename)
 df.to_csv(write_name)
---
+++ Preprocessing/rename_cols.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Rename data columns according to argument inputs")
+parser.add_argument('filename')
+parser.add_argument('-1', '--an1', type=str)
+parser.add_argument('-2', '--an2', type=str)
+parser.add_argument('-3', '--an3', type=str)
+parser.add_argument('-4', '--an4', type=str)
+parser.add_argument('-5', '--an5', type=str)
+parser.add_argument('-6', '--an6', type=str)
+parser.add_argument('-7', '--an7', type=str)
+parser.add_argument('-8', '--an8', type=str)
+parser.add_argument('-s', '--sampling', type=str, default='200')
+args = parser.parse_args()
+
+df = pd.read_csv(args.filename) \
+       .rename(columns={"Interval|\"ms\"|0|0|1" : "Interval (ms)",
+                        "Analog1|\"Volts\"|0.0|5.0|" + args.sampling : args.an1 or "Analog1|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog2|\"Volts\"|0.0|5.0|" + args.sampling : args.an2 or "Analog2|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog3|\"Volts\"|0.0|5.0|" + args.sampling : args.an3 or "Analog3|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog4|\"Volts\"|0.0|5.0|" + args.sampling : args.an4 or "Analog4|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog5|\"Volts\"|0.0|5.0|" + args.sampling : args.an5 or "Analog5|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog6|\"Volts\"|0.0|5.0|" + args.sampling : args.an6 or "Analog6|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog7|\"Volts\"|0.0|5.0|" + args.sampling : args.an7 or "Analog7|\"Volts\"|0.0|5.0|" + args.sampling,
+                        "Analog8|\"Volts\"|0.0|5.0|" + args.sampling : args.an8 or "Analog8|\"Volts\"|0.0|5.0|" + args.sampling
+                        })
+
+write_name = 'RENAMED_COLS_' + os.path.basename(args.filename)
+df.to_csv(write_name)
